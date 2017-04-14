c36b91c5de42a5350aa100d4301442b50f90ae86
Matthew Splett <msplett@tripwire.com>
2017-04-14 14:38:03 -0700

    needed to move things around so sprites didn't clobber name table.
    Now follows memory map as written in:
      Video Display Processors Programmer's Guide, by Texas Instruments.
    
    http://www.msxblog.es/wp-content/uploads/2009/09/vdpprogrammersguide.pdf

--- vdp_setbitmap.c
+++ vdp_setbitmap.c
@@ -5,10 +5,10 @@ int set_bitmap(int sprite_mode) {
 	int unblank = VDP_MODE1_16K | VDP_MODE1_UNBLANK | VDP_MODE1_INT | sprite_mode;
 	VDP_SET_REGISTER(VDP_REG_MODE0, VDP_MODE0_BITMAP);
 	VDP_SET_REGISTER(VDP_REG_MODE1, VDP_MODE1_16K);		// no need to OR in the sprite mode for now
-	VDP_SET_REGISTER(VDP_REG_SIT, 0x06);	gImage = 0x1800;
+	VDP_SET_REGISTER(VDP_REG_SIT, 0x0E);	gImage = 0x3800;
 	VDP_SET_REGISTER(VDP_REG_CT, 0xFF);		gColor = 0x2000;
 	VDP_SET_REGISTER(VDP_REG_PDT, 0x03);	gPattern = 0x0000;
-	VDP_SET_REGISTER(VDP_REG_SAL, 0x36);	gSprite = 0x1B00;	vdpchar(gSprite, 0xd0);
+	VDP_SET_REGISTER(VDP_REG_SAL, 0x76);	gSprite = 0x3B00;	vdpchar(gSprite, 0xd0);
 	VDP_SET_REGISTER(VDP_REG_SDT, 0x03);	gSpritePat = 0x1800;
 	nTextRow = 736;
 	nTextEnd = 767;
