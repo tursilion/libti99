1cf047da81cdd41753accad1af1bca7607c93188
tursilion <darkmane@harmlesslion.com>
2019-02-10 01:12:20 -0800

    Add explicit control of the 9902 control bits to rs232_setcontrol and rs232_setbps - this should fix the RS232 init code and get things running.

--- rs232_setbps.c
+++ rs232_setbps.c
@@ -7,11 +7,21 @@
 void rs232_setbps(int card, int uart, int bps) {
     int rawCRU = rs232raw_getuart(card, uart);
 
+	// you don't technically have to turn off the bits after a write - the
+	// chip is supposed to turn them off for you. But again, I'm thinking
+	// about buggy emulation and broken future chips. ;)
+
     __asm__ (
         "  mov %0,r12\n"	// get the rawcru address
-        "  sbo 12\n"		// request write to recv rate
-        "  ldcr %1,11\n"	// load recv rate (auto-decrements to send rate)
-        "  ldcr %1,11\n"	// load send rate (Nouspikel says you can do both at once, but safer this way)
+        "  sbz 14\n"		// not request to write control word
+        "  sbz 13\n"		// not request to write interval register
+        "  sbo 12\n"		// request write to rx rate
+        "  sbz 11\n"		// not request to write tx data rate
+        "  ldcr %1,11\n"	// load rx rate (auto-decrements to send rate)
+        "  sbz 12\n"		// not request write to rx rate
+        "  sbo 11\n"		// request to write tx data rate
+        "  ldcr %1,11\n"	// load tx rate (Nouspikel says you can do both at once, but safer this way)
+        "  sbz 11\n"		// and turn it off too
         : : "r" (rawCRU), "r" (bps) : "r12" 
     );
 
--- rs232_setcontrol.c
+++ rs232_setcontrol.c
@@ -7,6 +7,14 @@
 void rs232_setcontrol(int card, int uart, int control) {
     int rawCRU = rs232raw_getuart(card, uart);
 
+	// Note: the reset is supposed to set the bits for loading:
+	// control register(14), interval register (13), rx rate (12)
+	// and tx rate (11). The highest one set is the one that is
+	// loaded. But in order to allow more flexibility with this
+	// library, as well as to deal with incomplete emulation and
+	// potential new devices that also emulate a 9902, we'll just
+	// be very selective and not make assumptions!
+
     __asm__ (
         "  mov %0,r12\n"	// get the rawcru address
         "  sbo 31\n"		// reset
@@ -17,8 +25,12 @@ void rs232_setcontrol(int card, int uart, int control) {
         "  sbz 17\n"		// clear abort/BREAK
         "  sbz 15\n"		// clear loopback test mode
         "  sbo 14\n"		// request to write control word
+        "  sbz 13\n"		// not request to write interval register
+        "  sbz 12\n"		// not request to write rx data rate
+        "  sbz 11\n"		// not request to write tx data rate
         "  swpb %1\n"		// get the byte into the msb
         "  ldcr %1,8\n"		// write it
+        "  sbz 14\n"		// and turn it off for later use
         "  swpb %1\n"		// fix the reg in case gcc wants it
         
         : : "r"(rawCRU), "r"(control) : "r12" 
