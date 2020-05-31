c771b70af56fb8974d894a1825fdd2f7f1282ade
tursilion <darkmane@harmlesslion.com>
2020-05-31 02:33:24 -0700

    Fix writestring to work with new flag system and all tile graphics modes

--- vdp_writestring.c
+++ vdp_writestring.c
@@ -1,11 +1,18 @@
 #include "vdp.h"
 
 void writestring(int row, int col, char *pStr) {
-  if (nTextEnd > 767) {
-    VDP_SET_ADDRESS_WRITE(VDP_SCREEN_TEXT(row,col));
-  } else {
-    VDP_SET_ADDRESS_WRITE(VDP_SCREEN_POS(row,col));
-  }
+	if (nTextFlags&TEXT_WIDTH_32) {
+		VDP_SET_ADDRESS_WRITE(VDP_SCREEN_POS(row,col)+gImage);
+	} else if (nTextFlags&TEXT_WIDTH_40) {
+		VDP_SET_ADDRESS_WRITE(VDP_SCREEN_TEXT(row,col)+gImage);
+	} else if (nTextFlags&TEXT_WIDTH_80) {
+		VDP_SET_ADDRESS_WRITE(VDP_SCREEN_TEXT80(row,col)+gImage);
+	} else if (nTextFlags&TEXT_WIDTH_64) {
+		// TODO: this doesn't make any sense, does it??
+		VDP_SET_ADDRESS_WRITE(VDP_SCREEN_TEXT64(row,col)+gImage);
+	} else {
+		return;
+	}
 	while (*pStr) {
 		VDPWD = *(pStr++);
 	}
